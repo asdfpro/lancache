9d445a487daa3c034f91dd1ed7757d51c17dda6a
asdfpro <xtremegamelan@gmail.com>
2016-07-06 01:22:24 -0400

    Wrong file location
    Renamed /etc/nginx/sites to /etc/nginx/vhosts

--- extras/lancache-uplay.sh
+++ extras/lancache-uplay.sh
@@ -2,7 +2,7 @@
 # Stop NGINX service
 service nginx stop
 # Add Uplay to lancache.conf
-cat >> /etc/nginx/sites/lancache.conf <<EOF
+cat >> /etc/nginx/vhosts/lancache.conf <<EOF
 # Uplay (Ubisoft)
 server {
 	listen lancache-uplay default;
