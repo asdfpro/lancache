387c63caa1bfbaa8bd799eedce89fe93fccbcc88
asdfpro <xtremegamelan@gmail.com>
2016-07-06 01:19:51 -0400

    Wrong file location
    Renamed /etc/nginx/sites to /etc/nginx/vhosts

--- extras/lancache-enmasse.sh
+++ extras/lancache-enmasse.sh
@@ -2,7 +2,7 @@
 # Stop NGINX service
 service nginx stop
 # Add En Masse to lancache.conf
-cat >> /etc/nginx/sites/lancache.conf <<EOF
+cat >> /etc/nginx/vhosts/lancache.conf <<EOF
 # ENMASSE
 server {
 	listen lancache-enmasse default;
