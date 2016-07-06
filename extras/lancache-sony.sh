19f52c6fc9f3d0e51abe8765e8bbe3c9c57c0123
asdfpro <xtremegamelan@gmail.com>
2016-07-06 01:21:26 -0400

    Wrong file location
    Renamed /etc/nginx/sites to /etc/nginx/vhosts

--- extras/lancache-sony.sh
+++ extras/lancache-sony.sh
@@ -2,7 +2,7 @@
 # Stop NGINX service
 service nginx stop
 # Add Sony to lancache.conf
-cat >> /etc/nginx/sites/lancache.conf <<EOF
+cat >> /etc/nginx/vhosts/lancache.conf <<EOF
 # Sony
 server {
 	listen lancache-sony default;
